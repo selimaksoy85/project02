b6cbaa1e9db4145e100cbb9ebbb2958ca448c03e
Selim Aksoy <selimaksoy85@hotmail.com>
2020-11-08 19:09:20 +0300

    second commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -3,7 +3,7 @@ package firstPackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-
 		System.out.println("first commit from local master branch");
+		System.out.println("second commit from local master");
 	}
 }
