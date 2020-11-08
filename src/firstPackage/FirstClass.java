32fa5b9a282479cd058799d131c1af1aa3b19f2b
selimaksoy85 <74103926+selimaksoy85@users.noreply.github.com>
2020-11-08 20:03:22 +0300

    third commit from remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -8,5 +8,6 @@ public class FirstClass {
 		System.out.println("third commit from local master");
 		System.out.println("first commit from remote master branch");
 		System.out.println("Second commit from remote master branch");
+		System.out.println("Third commit from remote master branch");
 	}
 }
