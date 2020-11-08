99a1bded30195607bc50581830b9c95a8efe7bcb
Selim Aksoy <selimaksoy85@hotmail.com>
2020-11-08 22:16:25 +0300

    first commit from local feature branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -16,5 +16,7 @@ public class FirstClass {
 
 		System.out.println("Here is another conflict from local master branch");
 		
+		System.out.println("Here are some changes from local feature branch");
+		
 	}
 }
