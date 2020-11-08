60cc1b90abeabdb74007f72f7339cbe9eb265081
Selim Aksoy <selimaksoy85@hotmail.com>
2020-11-08 19:14:08 +0300

    third commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -5,5 +5,6 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("first commit from local master branch");
 		System.out.println("second commit from local master");
+		System.out.println("third commit from local master");
 	}
 }
